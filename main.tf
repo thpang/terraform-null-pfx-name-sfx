f855d32191586a4385aa8ecfd3ce997ef61cccfb
Thomas S. Pangborn <thomas.pangborn@sas.com>
2023-03-08 08:54:43 -0500

    Added copyright

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+#
+# Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
 resource "null_resource" "entity" {
   triggers = {
     name = local.name
